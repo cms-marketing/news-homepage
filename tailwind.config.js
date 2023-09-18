a27547a7913f05e69967e43a6bef31f46e125b4a
じぇいど <83527821+jade-banares@users.noreply.github.com>
2023-09-18 12:45:14 +0800

    add markup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        sm: "375px",
+        md: "768px",
+        lg: "976px",
+      },
+      colors: {
+        softOrange: "hsl(35, 77%, 62%)",
+        softRed: "hsl(5, 85%, 63%)",
+        offWhite: "hsl(36, 100%, 99%)",
+        darkGrayishBlue: "hsl(236, 13%, 42%)",
+        veryDarkBlue: "hsl(240, 100%, 5%)",
+      },
+      fontFamily: {
+        inter: ["Inter-Regular", "sans-serif"],
+        interBold: ["Inter-Bold", "sans-serif"],
+        interExtraBold: ["Inter-ExtraBold", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
